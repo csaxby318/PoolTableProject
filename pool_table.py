f757edd6d04d19c18ec9f40cbbb43d96c28e9f3c
Clarence Saxby <csaxby318@gmail.com>
2021-02-11 16:46:06 -0500

    eliminated possible errors, refactored and added more notes

--- pool_table.py
+++ pool_table.py
@@ -6,10 +6,7 @@ tables = []
 
 # import datetime module to capture start, end and play time
 from datetime import datetime
-#create a readable format for the time
-#time_now = datetime.now()
-# formatted_time = time_now.strftime("%I:%M%p on %m-%d-%Y")
-# create a readable formate for the date
+# create a readable formate for the date only, later used to generate json log file name
 date_now = datetime.now()
 current_date = date_now.date()
 formatted_date = current_date.strftime("%m-%d-%Y")
@@ -55,10 +52,13 @@ def display_tables():
             print(f"\nTable {t.table_name} {is_available}")
         elif t.available == False:
             is_not_available = "is NOT available"
-            print(f"\nTable {t.table_name} {is_not_available} - Checkout Time: {t.start_time} - Play Time: {t.time_played()}")
+            formatted_start_time = t.start_time.strftime("%I:%M%p on %m-%d-%Y")
+            print(f"\nTable {t.table_name} {is_not_available} - Checkout Time: {formatted_start_time} - Play Time: {t.time_played()}")
 
 
 while True: 
+    # when that the admin sees
+    print(f"\n  Frankie's Billiards Club")
     menu = input("""
     1. Display all tables
     2. Check-out a table 
@@ -72,11 +72,16 @@ while True:
     
     # menu option 2 is to checkout a table    
     elif menu == "2":
+        # display all of the tables
         display_tables()
-        which_table = int(input("Checkout table number: "))
+        # ask the admin which table they would like to check
+        which_table = int(input(f"\nCheckout table number: "))
         table = pool_tables[which_table-1]
+        # if the table is already checked out, display message
         if table.available == False:
-            print(f"\n /// Pool Table {table.table_name} is currently occupied ///")
+            print(f"\n ******** PLEASE BE ADVISED ********")
+            print(f"/// Pool Table {table.table_name} is currently occupied ///")
+        # if table is not checked out, check it out
         else:
             table.check_out_table()
             display_tables()
@@ -88,30 +93,36 @@ while True:
             t = pool_tables[table]
             if t.available == False:
                 is_not_available = "is NOT available"
-                print(f"\nTable {t.table_name} {is_not_available} - Checkout Time: {t.start_time} - Play Time: {t.time_played()}")
-        # ask the admin which table they would like to check-in, then check-in that table
-        which_table = int(input("Check-in table number: "))
+                formatted_start_time = t.start_time.strftime("%I:%M%p on %m-%d-%Y")
+                print(f"\nTable {t.table_name} {is_not_available} - Checkout Time: {formatted_start_time} - Play Time: {t.time_played()}")
+        # ask the admin which table they would like to check-in
+        which_table = int(input(f"\nCheck-in table number: "))
         table = pool_tables[which_table-1]
-        table.check_in_table()
+        # if the table is already checked in, display message
+        if table.available == True:
+            print(f"\n ******** PLEASE BE ADVISED ********")
+        # if table is not checked in, check it in
+        else:
+            table.check_in_table()
         # format the start, end and play time for better view in the log file
-        formatted_start_time = table.start_time.strftime("%I:%M%p on %m-%d-%Y")
-        formatted_end_time = table.end_time.strftime("%I:%M%p on %m-%d-%Y")
-        play_time = table.time_played()
-        formatted_play_time = str(play_time)
-        # create/write to file the required info as the table is checked in
-        with open(f"{formatted_date}.json", "w") as file_object:
-            table = {"table": table.table_name, "started": formatted_start_time, "ended": formatted_end_time, "playtime": formatted_play_time}
-            tables.append(table)
-            json.dump(tables, file_object)
-    
-    
-    elif menu == "q":
+        try:
+            formatted_start_time = table.start_time.strftime("%I:%M%p on %m-%d-%Y")
+            formatted_end_time = table.end_time.strftime("%I:%M%p on %m-%d-%Y")
+            play_time = table.time_played()
+            formatted_play_time = str(play_time)
+        # create/write to json log file the required info as the table is checked in
+            with open(f"{formatted_date}.json", "w") as file_object:
+                table = {"table": table.table_name, "started": formatted_start_time, "ended": formatted_end_time, "playtime": formatted_play_time}
+                tables.append(table)
+                json.dump(tables, file_object)
+        # if the table is already checked in, display message
+        except AttributeError:
+            print(f"/// Pool Table {table.table_name} is already checked in ///")
+        # display all of the tables after check-in is completed     
+        display_tables()
+    # menu option to quit the app
+    elif menu == "q" or menu == "Q":
         break
-
+    # if an invalid choice is made at the menu, the below message will display
     else:
         print(f"{menu} is not a valid choice. Please try again")
-
-
-# pool_tables[2].check_out_table()
-
-# display_tables()
